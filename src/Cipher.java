33f1860ee2473f7bda5ad738f8385c5ebe271096
Kai <khir829@aucklanduni.ac.nz>
2022-07-03 22:33:11 +1200

    Fix bug to include special characters in cipher

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,13 +1,18 @@
 
 public class Cipher {
-	public static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+	private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+	private static final String specialChars = "/*!@#$%^&*()\"{}_[]|\\?/<>,.";
 
 	public static StringBuffer cipher(String message, int shift) {
 		message = message.toLowerCase();
 		StringBuffer result = new StringBuffer();
+
 		for (int i = 0; i < message.length(); i++) {
+			String stringValueOf = String.valueOf(message.charAt(i));
 			if (message.charAt(i) == ' ') {
 				result.append(' ');
+			} else if (specialChars.contains(stringValueOf)) {
+				result.append(message.charAt(i));
 			} else {
 				int charPos = alphabet.indexOf(message.charAt(i));
 				int pos = Math.abs((charPos + shift) % 26);
